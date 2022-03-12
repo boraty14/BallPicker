ea95132b40122d648bc1b08e74bcc159c48b116e
boraty <brataysen@gmail.com>
2022-03-12 18:38:37 +0300

    minor adjustments

--- Assets/Scripts/GameElements/Collectable.cs
+++ Assets/Scripts/GameElements/Collectable.cs
@@ -1,4 +1,5 @@
 using System;
+using Core;
 using DG.Tweening;
 using GameSettings;
 using UnityEngine;
@@ -9,6 +10,30 @@ namespace GameElements
     {
         [SerializeField] private SCollectableSettings collectableSettings;
         private bool _didHitPlatform = false;
+
+        private void OnEnable()
+        {
+            EventBus.OnLevelWin += OnLevelWin;
+            EventBus.OnLevelLose += OnLevelLose;
+        }
+
+        private void OnDisable()
+        {
+            EventBus.OnLevelWin -= OnLevelWin;
+            EventBus.OnLevelLose -= OnLevelLose;
+        }
+
+        private void OnLevelWin()
+        {
+            transform.DOKill();
+            Destroy(gameObject);
+        }
+
+        private void OnLevelLose()
+        {
+            transform.DOKill();
+            Destroy(gameObject);
+        }
 
         public bool HitMovingPlatform()
         {
